3ba079f16f49f27e8013545527bce7350ca78201
Sean4101 <seanchen1014@gmail.com>
2024-02-21 00:41:05 +0800

    Updated cross-compile.txt to meet the requirement; Added fileresult.txt; Revised writer.c to change the printf to syslog.

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <syslog.h>
 
 int main(int argc, char *argv[]) {
+
+    openlog(NULL, 0, LOG_USER);
     
     if (argc != 3) {
-        printf("Usage: %s <writefile> <writestr>\n", argv[0]);
+        syslog(LOG_ERR, "Usage: %s <file> <content>", argv[0]);
         return 1;
     }
+
+    syslog(LOG_INFO, "Writing %s to file %s", argv[2], argv[1]);
 
     FILE *file;
 
     file = fopen(argv[1], "w");
 
     if (file == NULL) {
-        printf("Error opening file!\n");
+        syslog(LOG_ERR, "Error opening file %s", argv[1]);
         return 1;
     }
 
