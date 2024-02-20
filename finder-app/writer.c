2f76f44720e2e68f8c8c9bdbbb98df301ffc50c7
Sean4101 <seanchen1014@gmail.com>
2024-02-20 23:14:49 +0800

    writer.c can now create and write files, but syslog is still not yet implemented

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    printf("Hello, World!\n");
+    
+    if (argc != 3) {
+        printf("Usage: %s <writefile> <writestr>\n", argv[0]);
+        return 1;
+    }
+
+    FILE *file;
+
+    file = fopen(argv[1], "w");
+
+    if (file == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+
+    fprintf(file, "%s", argv[2]);
+
+    fclose(file);
+
     return 0;
 }
